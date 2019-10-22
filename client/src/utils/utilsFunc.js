4abaf652ecfbd1c09e5b2cf94776b6d603302dec
Min Khant Kyaw <minkhantkyaw.na@gmail.com>
2019-10-22 13:39:39 -0400

    fixed the getDuration function error

--- client/src/utils/utilsFunc.js
+++ client/src/utils/utilsFunc.js
@@ -3,16 +3,16 @@ const getDuration = input => {
   let roundTimeDiff = Math.round(timeDiff);
 
   return timeDiff < 1
-    ? `${Math.round(timeDiff < 0 ? 0 : timeDiff * 60)} sec ago`
-    : timeDiff === 1
+    ? `${timeDiff < 0.5 ? "now" : Math.round(timeDiff * 60) + "sec ago"}`
+    : timeDiff < 2
     ? `1 min ago`
     : timeDiff < 60
     ? `${roundTimeDiff} mins ago`
-    : timeDiff < 90
+    : timeDiff < 120
     ? `1 hour ago`
-    : timeDiff <= 60 * 24
+    : timeDiff < 60 * 24
     ? `${Math.round(roundTimeDiff / 60)} hours ago`
-    : timeDiff <= 60 * 24 * 1.5
+    : timeDiff < 60 * 24 * 2
     ? `1 day ago`
     : `${Math.round(roundTimeDiff / 60 / 24)} days ago `;
 };
